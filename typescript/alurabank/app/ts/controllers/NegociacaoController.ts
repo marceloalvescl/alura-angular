9aa9dcf5f3c88558d9a9d910f8e10a5eeb03f170
Marcelo <marceloalvescl@sempreceub.com>
2019-11-17 12:31:16 -0300

    template dinâmico, atualizando tabela

--- typescript/alurabank/app/ts/controllers/NegociacaoController.ts
+++ typescript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -4,11 +4,13 @@ class NegociacaoController{
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor= <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event){
@@ -19,10 +21,7 @@ class NegociacaoController{
             parseFloat(this._inputValor.value),
         );
         this._negociacoes.adiciona(negociacao);
-        this._negociacoes.pegaNegociacoes().forEach(negociacao => {
-                console.log(negociacao.data);
-                console.log(negociacao.quantidade);
-                console.log(negociacao.valor);
-            })
+        
+        this._negociacoesView.update(this._negociacoes);
     }
 }
